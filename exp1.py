fd4ff9b9154a9d296a9ce2c847438d324b5879ec
Noorjaha479 <noorjahaansari253@gmail.com>
2026-02-13 15:33:12 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 14:56:12 2026
+
+@author: User
+"""
+
+name=input("enter your name:")
+age =input("enter your age:")
+student_class=input("enter your class:")
+division=input("enter your division:")
+roll_no=input("enter your roll number:")
+batch=input("enter your batch:")
+phone_no=input("enter your phone number:")
+blood_group=input("enter your blood group:")
+address=input("enter your address:")
+college=input("enter your college name:")
+city=input("enter your city:")
+print("\n ____student information____")
+
+print("Name:",name)
+print("Age:",age)
+print("Class:",student_class)
+print("Division:",division)
+print("Roll Number:",roll_no)
+print("Batch:",batch)
+print("Phone Number:",phone_no)
+print("Blood Group:", blood_group)
+print("Address:",address)
+print("College:",college)
+print("City:",city)
+
+
